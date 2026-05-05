5a112edd239b7df2e331d3741d45ec4f16701090
Tommy <hoangcongduyhy@gmail.com>
2026-05-05 07:15:11 +0000

    [Must be revert] Debug email service

--- auth-service/internal/service/user_service.go
+++ auth-service/internal/service/user_service.go
@@ -9,6 +9,7 @@ import (
 	"encoding/hex"
 	"errors"
 	"fmt"
+	"log"
 	"os"
 	"time"
 
@@ -212,6 +213,7 @@ func (s *UserService) ConfirmEmail(token string) error {
 
 // ResendConfirmationEmail resends the confirmation email to a user.
 func (s *UserService) ResendConfirmationEmail(email string) error {
+	log.Printf("[DEBUG-QUEUE] Nhan request: '%s'", email)
 	user, err := s.repo.FindByEmail(email)
 	if err != nil {
 		return apperrors.NewUserService("resend_confirmation_email", "user not found", err)
@@ -227,10 +229,11 @@ func (s *UserService) ResendConfirmationEmail(email string) error {
 
 	if s.emailService != nil {
 		appEnv := os.Getenv("APP_ENV")
-		send_to := os.Getenv("SMTP_TO")
-		if appEnv == "" {
-			send_to = user.Email
+		send_to := user.Email
+		if appEnv == "development" {
+			send_to = os.Getenv("SMTP_TO")
 		}
+		log.Printf("[DEBUG-QUEUE] Nhan request: '%s'", send_to)
 		if err := s.emailService.SendRegistrationConfirmation(send_to, *user.ConfirmationToken); err != nil {
 			return err
 		}
